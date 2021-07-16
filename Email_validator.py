bae1ca70498e7093081d65b84a0b25c5ff67d0a3
vesso8 <skittles8@abv.bg>
2021-07-16 17:58:01 +0300

    errors

---
+++ Email_validator.py
@@ -0,0 +1,21 @@
+from custom_exeptions import NameTooShort, MustContainAtSymbolError, InvalidDomainError
+from helpers import VALID_DOMAINS
+email = input()
+def valid_email(email):
+    try:
+        name, domain = email.split("@")
+    except ValueError:
+        raise MustContainAtSymbolError("Email must contain @")
+    try:
+        name, extension = domain.split('.')
+    except ValueError:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+    if extension not in VALID_DOMAINS:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+    if len(name) < 4:
+        raise NameTooShort("Name must be more than 4 characters")
+
+while not email == "End":
+    if valid_email(email):
+        print("Email is valid")
+    email = input()
---
+++ Numbers Dictionary.py
@@ -0,0 +1,32 @@
+numbers_dictionary = {}
+
+while True:
+    number_as_string = input()
+    if "Search" == number_as_string:
+        break
+    try:
+        number = int(input())
+        numbers_dictionary[number_as_string] = number
+    except ValueError:
+        print("The variable number must be an integer")
+line = input()
+
+while line != "Remove":
+    searched = line
+    try:
+        print(numbers_dictionary[searched])
+    except KeyError:
+        print("Number does not exist in dictionary")
+    line = input()
+
+line = input()
+
+while line != "End":
+    searched = line
+    try:
+        del numbers_dictionary[searched]
+    except KeyError:
+        print("Number does not exist in dictionary")
+    line = input()
+
+print(numbers_dictionary)
